e4bdfc82b7fdaf5a66c745d28e65dec9f2bd49fc
Iago <iago.almeida@visie.com.br>
2025-04-17 23:50:21 -0300

    ✨ inicio reestruturacao masmorras

---
+++ app/data/missoes.py
@@ -0,0 +1,11 @@
+MISSOES = {
+    'floresta': (0, 25, 'Rato Guerreiro', False),
+    'mata_fechada': (0, 30, 'Gnomo Ancião', False),
+    'castelo_abandonado': (0, 35, 'Entidade Obscura', False),
+    'cemiterio': (0, 40, 'Esqueleto', False),
+    'catacumbas': (0, 40, 'Protetor das Catacumbas', False),
+    'calabouco': (0, 50, 'Bispo Corrompido', False),
+    'laboratorio_secreto': (0, 100, 'Dragão Jovem', False),
+    'submundo': (0, 200, 'Experimento IV', False),
+    'nulo': (0, 250, 'Amon', False)
+}
